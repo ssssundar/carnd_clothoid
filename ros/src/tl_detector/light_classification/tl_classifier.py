6fb1c8c7b6de51b9a872a88428fc91b4f1027829
Yufei Liu <yufeiliu@yufeiliu2.mtv.corp.google.com>
2018-07-21 15:46:15 -0700

    Add inference logit.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,31 @@
+import numpy as np
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
 
 class TLClassifier(object):
-    def __init__(self):
-        #TODO load classifier
-        pass
+    def __init__(self, is_sim):
+        classifier_model = 'light_classification/classifiers/sim_model_classifier/frozen_inference_graph.pb'
+        if not is_sim:
+            classifier_model = 'light_classification/classifiers/real_model_classifier/frozen_inference_graph.pb'
+        # TODO: experiment with different thresholds to pick the best
+        self.threshold = .5
+
+        self.graph = self._load_graph(classifier_model)
+        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+        self.scores = self.graph.get_tensor_by_name('detection_scores:0')
+        self.classes = self.graph.get_tensor_by_name('detection_classes:0')
+        self.sess = tf.Session(graph=self.graph)
+
+    def _load_graph(frozen_graph_filename):
+        with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+ 
+        with tf.Graph().as_default() as graph:
+            tf.import_graph_def(graph_def, name='')
+
+        return graph
+ 
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +37,21 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        with self.graph.as_default():
+            image_expanded = np.expand_dims(image, axis=0)
+            (scores, classes) = self.sess.run(
+                [self.scores, self.classes],
+                feed_dict={self.image_tensor: image_expanded})
+
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+
+        if (len(scores) > 0) and (scores[0] > self.threshold):
+            if classes[0] == 1:
+                return TrafficLight.Green
+            if classes[0] == 2:
+                return TrafficLight.Red
+            if classes[0] == 3:
+                return TrafficLight.Yellow
+
         return TrafficLight.UNKNOWN
